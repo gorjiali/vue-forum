5d04f85f3603b1bd477e9119d5223fa91dc25fcd
Ali Gorji <gorjiali@hotmail.com>
2020-07-04 19:15:18 +0430

    feat: create new post

--- src/views/ThreadShow.vue
+++ src/views/ThreadShow.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="col-large push-top">
     <h1>{{ thread.title }}</h1>
+
     <PostList :posts="posts" />
+
+    <form @submit.prevent="addPost">
+      <div class="form-group">
+        <textarea v-model="newPostText" cols="30" rows="10" class="form-input"></textarea>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">Submit post</button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -17,14 +27,15 @@ export default {
     }
   },
 
-  components: {
-    PostList
-  },
-
   data() {
     return {
-      thread: sourceData.threads[this.id]
+      thread: sourceData.threads[this.id],
+      newPostText: ""
     };
+  },
+
+  components: {
+    PostList
   },
 
   computed: {
@@ -33,6 +44,26 @@ export default {
       return Object.values(sourceData.posts).filter(post =>
         postIds.includes(post[".key"])
       );
+    }
+  },
+
+  methods: {
+    addPost() {
+      const postId = `newPost ${Math.random()}`;
+      const post = {
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now()) / 1000,
+        userId: "ALXhxjwgY9PinwNGHpfai6OWyDu2",
+        threadId: this.id,
+        ".key": postId
+      };
+
+      // this.$set(obj, propertyName, value)
+      this.$set(this.thread.posts, postId, postId);
+      this.$set(sourceData.posts, postId, post);
+      this.$set(sourceData.users[post.userId].posts, postId, postId);
+
+      this.newPostText = "";
     }
   }
 };
