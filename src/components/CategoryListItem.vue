66c6811aeb895f0a26d0cf18d40e2e851fadac08
Ali Gorji <gorjiali@hotmail.com>
2020-07-06 10:43:06 +0430

    feat: create category list and item and show in home page

---
+++ src/components/CategoryListItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="forum-list">
+    <h2 class="list-title">
+      <a href="#">{{ category.name }}</a>
+    </h2>
+    <ForumListItem v-for="forum in forums" :key="forum['.key']" :forum="forum" />
+  </div>
+</template>
+
+<script>
+import ForumListItem from "./ForumListItem";
+import sourceData from "@/data";
+
+export default {
+  props: {
+    category: {
+      type: Object,
+      required: true
+    }
+  },
+
+  components: {
+    ForumListItem
+  },
+
+  computed: {
+    forums() {
+      return Object.values(sourceData.forums).filter(
+        forum => forum.categoryId === this.category[".key"]
+      );
+    }
+  }
+};
+</script>
+
+<style>
+</style>
